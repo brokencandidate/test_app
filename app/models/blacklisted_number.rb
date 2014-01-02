9ab77411f33403442482b0f7a5d3e7582215e801
Steven Williamson <steve.williamson@dialogue.net>
2014-01-02 16:57:40 +0000

    WIP

---
+++ app/models/blacklisted_number.rb
@@ -0,0 +1,4 @@
+class BlacklistedNumber < ActiveRecord::Base
+  self.table_name = "BLACKLISTED_NUMBERS"
+  self.primary_key = "ID"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ FirstApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'blacklisted_number#index'
+
+   resources :blacklisted_number
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
